84687396a87f24e888454ac18a6de4eec664f31a
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2020-11-22 20:52:07 -0500

    modules + express setup

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http = require('http');
+const express = require('express')
+const cors = require('cors')
+const index = require('./routes/index');
 const { mainDBMigrations } = require('./dbs/main_migrations')
 const { authDBMigrations } = require('./dbs/auth_migrations')
+
+const app = express()
+app.use(cors())
+app.use('/index',index)
 
 const hostname = '0.0.0.0';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!\n')
-});
+app.listen(port, hostname, () => {
+  console.log(`App running at http://${hostname}:${port}`)
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
-});
-
-mainDBMigrations()
-authDBMigrations()
+// mainDBMigrations()
+// authDBMigrations()
